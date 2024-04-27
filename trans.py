77c4a83da0fce9200bd367dd52a9d7eddba79aff
huxx <huxxshadowhzy@gmail.com>
2024-04-27 20:59:25 +0800

    Final update

--- trans.py
+++ trans.py
@@ -1,11 +1,7 @@
 import os
 from pydub import AudioSegment
-
-import os
-from pydub import AudioSegment
-
 def trans_all_file(files_path, target="wav"):
-    output_dir = os.path.join(files_path, "../out")
+    output_dir = os.path.join(files_path, "../input/wavInput")
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -14,11 +10,10 @@ def trans_all_file(files_path, target="wav"):
         base_name, ext = os.path.splitext(filepath)
         if ext:
             song = AudioSegment.from_file(full_path, format=ext.replace(".", ""))
-            # 修改target_path来包含output_dir
             target_path = os.path.join(output_dir, f"{base_name}.{target}")
             song.export(target_path, format=target)
 
-trans_all_file("/MuiscGenreClassification/input")
+# trans_all_file("input")
 
 def make_chunks(audio_segment, chunk_length_ms):
     chunks = []
@@ -29,16 +24,33 @@ def make_chunks(audio_segment, chunk_length_ms):
     return chunks
 
 
-input_dir = "/MuiscGenreClassification/out"
-output_dir = "/MuiscGenreClassification/SimpleData/test"
+input_dir = "input/wavInput"
+output_dir = "SimpleData/test"
 
 
-size = 6000
-for each in os.listdir(input_dir):
-    if each.endswith(".wav"):
-        wave_path = os.path.join(input_dir, each)
-        wave = AudioSegment.from_file(wave_path, "wav")
-        chunks = make_chunks(wave, size)
-        for i, chunk in enumerate(chunks):
-            chunk_name = f"{each.split('.')[0]}-{i}.wav"
-            chunk.export(os.path.join(output_dir, chunk_name), format="wav")
+def start():
+    size = 6000
+    trans_all_file("input")
+    for each in os.listdir(input_dir):
+        if each.endswith(".wav"):
+            wave_path = os.path.join(input_dir, each)
+            wave = AudioSegment.from_file(wave_path, "wav")
+            chunks = make_chunks(wave, size)
+            for i, chunk in enumerate(chunks):
+                t=each.split('.')
+                chunk_name = f"{str().join(t[0:len(t)-1])}-{i}.wav"
+                chunk.export(os.path.join(output_dir, chunk_name), format="wav")
+
+def removeAll(filepath):
+    del_list = os.listdir(filepath)
+    for f in del_list:
+        file_path = os.path.join(filepath, f)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+
+def endall():
+    removeAll("SimpleData/test")
+    removeAll("input/wavInput")
+    print("End")
+
+endall()
